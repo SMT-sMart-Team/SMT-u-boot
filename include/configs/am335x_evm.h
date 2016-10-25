91e31a6a14c9e4203fb9b0f9e1b54b3374c9937a
roger zhao <general_zyj@163.com>
2016-10-25 14:50:32 +0800

    disable console on ttyS0

--- include/configs/am335x_evm.h
+++ include/configs/am335x_evm.h
@@ -114,7 +114,6 @@
 	"bootdir=/boot\0" \
 	"bootfile=zImage\0" \
 	"fdtfile=undefined\0" \
-	"console=ttyO0,115200n8\0" \
 	"partitions=" \
 		"uuid_disk=${uuid_gpt_disk};" \
 		"name=rootfs,start=2MiB,size=-,uuid=${uuid_gpt_rootfs}\0" \
